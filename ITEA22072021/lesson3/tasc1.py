6156661f220ebb3af51d703c8dc66f077c4945bf
oleh <shnypkooleg@gmail.com>
2021-07-28 19:59:59 +0300

    lesson3 test

--- ITEA22072021/lesson3/tasc1.py
+++ ITEA22072021/lesson3/tasc1.py
@@ -1,3 +1,21 @@
-a=1
-print(a)
-print(3)
+import json
+#
+# def print_file(file_name: str)-> None:
+#     with open
+
+#conver json
+def demo_json():
+    example_data ={
+        'name':"Alex",
+        'age':28,
+        'is_teacher': True,
+        'other':None,
+        'tags':"['Python','C++','Django']"
+    }
+
+    output_json_string= json.dumps(example_data)
+    print(example_data)
+    print(output_json_string)
+    print(type(output_json_string))
+
+demo_json()
